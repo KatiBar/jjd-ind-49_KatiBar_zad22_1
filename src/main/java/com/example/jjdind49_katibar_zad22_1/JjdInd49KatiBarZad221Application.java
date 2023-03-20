64779127ea5669af733d31f70adbd178eb137047
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-03-20 18:35:09 +0100

    Init commit

---
+++ src/main/java/com/example/jjdind49_katibar_zad22_1/JjdInd49KatiBarZad221Application.java
@@ -0,0 +1,13 @@
+package com.example.jjdind49_katibar_zad22_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd49KatiBarZad221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdInd49KatiBarZad221Application.class, args);
+    }
+
+}
